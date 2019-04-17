5a2a1d0747713f698d8299a621f034867c14484b
张熙沐枫 <zhangximufeng@gmail.com>
2019-04-17 13:29:01 +0800

    Updates

---
+++ precache-manifest.b03f20853e64f6055536acc98def6616.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b4a35c001060373a916",
+    "url": "./static/js/3.2b4a35c0.chunk.js"
+  },
+  {
+    "revision": "1d5f2d4b520845fc6a79",
+    "url": "./static/css/main.bcbfc121.chunk.css"
+  },
+  {
+    "revision": "3e0478c328ee511b633b",
+    "url": "./static/js/1.3e0478c3.chunk.js"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "./static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "0269d2eff05f9bd3ed27",
+    "url": "./static/js/2.0269d2ef.chunk.js"
+  },
+  {
+    "revision": "defb98583257610e959baa67ab0fa53b",
+    "url": "./static/media/beauty.defb9858.jpg"
+  },
+  {
+    "revision": "1d5f2d4b520845fc6a79",
+    "url": "./static/js/main.1d5f2d4b.chunk.js"
+  },
+  {
+    "revision": "3938082d00b2fde4b3f6",
+    "url": "./static/js/runtime~main.3938082d.js"
+  },
+  {
+    "revision": "553c69e9bcf06d9f6218730fc13fcdda",
+    "url": "./static/media/b1.553c69e9.jpg"
+  },
+  {
+    "revision": "2b4a35c001060373a916",
+    "url": "./static/css/3.5846192a.chunk.css"
+  },
+  {
+    "revision": "0269d2eff05f9bd3ed27",
+    "url": "./static/css/2.982a77f0.chunk.css"
+  },
+  {
+    "revision": "c98f20eb878f27cd7c0ead6ad334b2d8",
+    "url": "./index.html"
+  }
+];
